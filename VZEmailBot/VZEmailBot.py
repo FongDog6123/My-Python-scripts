3dcf13b5cd66d17efec660d5b51f6b944dfc388b
FongDog6123 <chris.abidog@gmail.com>
2019-08-24 21:57:43 -0700

    Update VZEmailBot.py

--- VZEmailBot/VZEmailBot.py
+++ VZEmailBot/VZEmailBot.py
@@ -9,31 +9,37 @@ from slacker import Slacker
 
 
 slack_api_token = creds.bot_token
-web_hook_url = 'https://hooks.slack.com/services/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'
+web_hook_url = 'your_webhook_url'
 slack = Slacker(slack_api_token)
 slack_msg = {'text': 'VZ email script activated!'}
 four = {'text': 'Running 4pm daily email task'}
 two = {'text': ' Running Mon 2am email task'}
 twopm = {'text': 'Running Mon 2pm email task'}
 
+#Request would post that script was ran successfully
 requests.post(web_hook_url,data=json.dumps(slack_msg))
 
 def job():
     requests.post(web_hook_url,data=json.dumps(four))
-    os.system('sh /Users/christopher.abidog/workspace/noc/VZEmailBot/emailscript.sh')
-    slack.files.upload('emailscript.txt', channels=['CLGCXUSPJ'])
+    #Post to slack channel that job at 4 PM task is running
+    os.system('path/to/shell/script')
+    #Post output that was redirected form stdout to text file to slack channel
+    slack.files.upload('emailscript.txt', channels=['dest_channel_id'])
 
 def job1():
     requests.post(web_hook_url,data=json.dumps(two))
-    os.system('sh /Users/christopher.abidog/workspace/noc/VZEmailBot/mon_weekly_start.sh)')
-    slack.files.upload('mon_weekly_start.txt', channels=['CLGCXUSPJ'])
+    #Post to slack channel that 2 AM task is running
+    os.system('path/to/shell/script)')
+     #Post output that was redirected form stdout to text file to slack channel
+    slack.files.upload('mon_weekly_start.txt', channels=['dest_channel_id'])
 
 def job2():
     requests.post(web_hook_url,data=json.dumps(twopm))
-    os.system('sh /Users/christopher.abidog/workspace/noc/VZEmailBot/mon_weekly_complete.sh)')
-    slack.files.upload('mon_weekly_complete.txt', channels=['CLGCXUSPJ'])
+    os.system('/path/to/shell/script')
+     #Post output that was redirected form stdout to text file to slack channel
+    slack.files.upload('mon_weekly_complete.txt', channels=['dest_channel_idJ'])
 
-
+#Use schedule module to have python script run at specified times
 schedule.every().day.at("16:01").do(job)
 schedule.every().monday.at("02:00").do(job1)
 schedule.every().monday.at("14:00").do(job2)
